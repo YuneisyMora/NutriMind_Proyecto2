bb2b453dd90ff084b42cd76150811c254af228a9
Yuneisy Mora <yunemora9@gmail.com>
2025-07-31 15:47:06 -0600

    [Creacion] Primer commit del proyecto 2 NutriMind

---
+++ NutriMind/src/main/java/ac/cr/uned/nutrimind/NutriMind.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ac.cr.uned.nutrimind;
+
+/**
+ *
+ * @author micha
+ */
+public class NutriMind {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
