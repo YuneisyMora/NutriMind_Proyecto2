737ebe89e23026a8ecf7ccfcd54ec4fae191768f
Yuneisy Mora <yunemora9@gmail.com>
2025-08-09 22:04:23 -0600

    [Actualizacion] GUI creada para varios roles

--- NutriMind/src/ac/cr/uned/nutrimind/NutriMind.java
+++ NutriMind/src/ac/cr/uned/nutrimind/NutriMind.java
@@ -2,7 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package nutrimind;
+package ac.cr.uned.nutrimind;
+
+import ac.cr.uned.nutrimind.vistas.Login_vista;
+import javax.swing.SwingUtilities;
 
 /**
  * 
@@ -16,6 +19,13 @@ public class NutriMind {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        SwingUtilities.invokeLater(() -> {
+            
+            Login_vista loginWindow = new Login_vista();
+            loginWindow.setLocationRelativeTo(null);
+            loginWindow.setResizable(false);
+            loginWindow.setVisible(true);
+        });
     }
     
 }
